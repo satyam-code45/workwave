574206d9556ee70e15af2ccfdaba1daafb7baf2a
satyam-code45 <satyam45.dev@gmail.com>
2025-03-23 23:55:19 +0530

    added postinstall prisma generate

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -1,7 +1,7 @@
-import Prisma from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: Prisma.PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new Prisma.PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
